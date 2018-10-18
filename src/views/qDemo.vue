a74f062703719b1dfd4d44d69a675c24d91c0f5b
cag2050 <cag2050@163.com>
2018-10-18 17:13:35 +0800

    m

--- src/views/qDemo.vue
+++ src/views/qDemo.vue
@@ -9,11 +9,42 @@ import q from 'q'
 
 export default {
     mounted () {
-        this.qConsole()
+        // this.qConsole1()
+        this.qConsole2()
     },
     methods: {
-        qConsole () {
-            console.log(q)
+        // 使用方法1：
+        // q.Promise((resolve, reject) => {
+        qConsole1 () {
+            q.Promise((resolve, reject) => {
+                throw new Error('error info')
+                resolve('xxx')
+            })
+                .then((value) => {
+                    console.log(value)
+                })
+                .catch(err => {
+                    console.log(err)
+                    // throw err
+                })
+                .finally(() => {
+                    console.log('finally')
+                    throw new Error('error in finally')
+                })
+                .done()
+        },
+        // 使用方法2：
+        // q.resolve().then()
+        qConsole2 () {
+            q.resolve()
+                .then(() => {
+                    return q.Promise((resolve, reject) => {
+                        resolve('2222')
+                    })
+                })
+                .then((value) => {
+                    console.log(value)
+                })
         }
     }
 }
